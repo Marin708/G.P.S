0d52209cc9eca0b606eb4a5bf04a9db514768c93
Gustavo <maringustavo1693@gmail.com>
2025-10-21 18:09:11 -0500

    Se suben nuevos cambios

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package edu.caldas.gps;
+package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class GpsApplication {
+public class DemoApplication {
     public static void main(String[] args) {
-      SpringApplication.run(GpsApplication.class, args);
+      SpringApplication.run(DemoApplication.class, args);
     }
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
       return String.format("Hello %s!", name);
     }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package edu.caldas.gps;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GpsApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
